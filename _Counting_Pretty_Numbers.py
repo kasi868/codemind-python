62e6897aac224fd9982616bc477d00ae1e01262f
Anuboyina Kasi Viswanath <79613729+kasi868@users.noreply.github.com>
2022-09-07 14:24:55 +0530

    n=int(input()) for i in range (n):     a,b=map(int,input().split())     c=0     for j in range (a,b+1):         d=j%10         if(d==2 or d==3 or d==9):            c+=1     print(c)

---
+++ _Counting_Pretty_Numbers.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range (n):
+    a,b=map(int,input().split())
+    c=0
+    for j in range (a,b+1):
+        d=j%10
+        if(d==2 or d==3 or d==9):
+           c+=1
+    print(c)

099812f7a75235fdfebcf8e88efac66d13ff8da4
Anuboyina Kasi Viswanath <79613729+kasi868@users.noreply.github.com>
2022-11-03 23:08:53 +0530

    def fun(a,b):     s=t=0     for i in range(1,a):         if a%i==0:             s+=i     for j in range(1,b):         if b%j==0:             t+=j     if s==b and t==a:         print("Amicable")     else:         print("Not Amicable") a=int(input()) b=int(input()) fun(a,b)

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,15 @@
+def fun(a,b):
+    s=t=0
+    for i in range(1,a):
+        if a%i==0:
+            s+=i
+    for j in range(1,b):
+        if b%j==0:
+            t+=j
+    if s==b and t==a:
+        print("Amicable")
+    else:
+        print("Not Amicable")
 a=int(input())
 b=int(input())
-s=0
-t=0
-for i in range(1,a+1):
-    if a%i==0:
-        s+=i
-for j in range(1,b+1):
-    if b%j==0:
-        t+=j
-if s==t:
-    print("Amicable")
-else:
-    print("Not Amicable")
+fun(a,b)

6f6f2540ef7d13a2c068f802eebff067553854bc
Anuboyina Kasi Viswanath <79613729+kasi868@users.noreply.github.com>
2022-11-04 06:42:51 +0530

    n=int(input()) v=n*n x=0 while(n>0):     r=n%10     x=x*10+r     n=n//10 a=x*x k=0 while(a>0):     b=a%10     k=k*10+b     a=a//10 if(v==k):     print('True') else:     print('False')

---
+++ Adam_number.py
@@ -0,0 +1,17 @@
+n=int(input())
+v=n*n
+x=0
+while(n>0):
+    r=n%10
+    x=x*10+r
+    n=n//10
+a=x*x
+k=0
+while(a>0):
+    b=a%10
+    k=k*10+b
+    a=a//10
+if(v==k):
+    print('True')
+else:
+    print('False')
